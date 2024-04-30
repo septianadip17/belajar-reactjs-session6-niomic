d53967a606f679712b6af7af933c99e18578130a
iay <septianadip7998@gmail.com>
2024-05-01 06:38:19 +0700

    done

--- src/appRouter.js
+++ src/appRouter.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import App from "./components/home/App";
 import Contact from "./components/page/Contact";
 
 const AppRouter = () => {
-  <BrowserRouter>
-    <div>
-      <Route path="/" exact component={App} />
-      <Route path="/contact" component={Contact} />
-    </div>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom/client";
 import AppRouter from "./appRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppRouter />);
+root.render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>
+);
